d75ead8036ef15d1cea5bfe3088d66d9a2df2bb2
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-08-31 09:11:44 +0700

    Finish for 30 Aug

--- src/Components/Todo/TodoList.jsx
+++ src/Components/Todo/TodoList.jsx
@@ -1,8 +1,24 @@
 import style from './TodoLists.module.scss'
+import { FaTrash, FaPen, FaRegCircle } from 'react-icons/fa';
 
 function TodoList() {
     return (
-        <div>TodoList</div>
-    )
+        <ul className={style}>
+            <li className={style.todo}>
+                <span className={style.todo__checkbox}>
+                </span>
+                <p className={style.todo__task}>TodoItem 1</p>
+                <span className={style.todo__date}>30 AUG</span>
+                <div className={style.todo__action}>
+                    <span className={style.todo__edit}>
+                        <FaPen />
+                    </span>
+                    <span className={style.todo__delete}>
+                        <FaTrash />
+                    </span>
+                </div>
+            </li>
+        </ul>
+    );
 }
 export default TodoList
