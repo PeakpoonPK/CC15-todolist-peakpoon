99ba39e87c226d355605561c048f54ca3496b8bd
PeakpoonPK <peakpoonamoroll@gmail.com>
2023-08-31 13:44:05 +0700

    add cancel to Cancel

--- src/Components/Common/Button/Button.jsx
+++ src/Components/Common/Button/Button.jsx
@@ -1,6 +1,14 @@
 import styles from './Button.module.scss';
 
-export function Button({ text, active = true }) {
+/* 
+*/
+export function Button({ type, onClick, text, active = true }) {
+  console.log(onClick);
   let btnStyles = active ? styles.btn__primary : styles.btn__secondary;
-  return <button className={`${styles.btn} ${btnStyles}`}>{text}</button>;
+  return (<button className={`${styles.btn} ${btnStyles}`}
+    type={type}
+    onClick={onClick}
+  >
+    {text}
+  </button>);
 }
--- src/Components/Todo/TodoForm.jsx
+++ src/Components/Todo/TodoForm.jsx
@@ -1,26 +1,61 @@
 import styles from './TodoForm.module.scss';
 import { Button } from '../Common/Button/Button'
+import { useState } from 'react';
 /*
 props ={
   textSubmit : string
 }
+
+CC1 -From Handle
+- FN contact Event is called "onSubmit"
+- FN was called by Browser (when?) by send parameter 1 option (event Object)
+- all Default  in <from> it do to submit
+- Sole: declare type of button
+      <Button type='button'>1</Button>
+          <Button type='Submit' >2</Button>
 */
 
 function TodoForm(props) {
+  const [isError, setIsError] = useState(true);
+
+  const handleSubmit = function (event) {
+    event.preventDefault();
+    console.log('submit');
+    setIsError(!isError);
+
+  };
+
+  const handleCancele = function (event) {
+    event.preventDefault();
+    console.log('cancele');
+
+  };
   return (
-    <form className={styles.todo__form__container}>
+
+    <form onSubmit={handleSubmit}
+      className={styles.todo__form__container}
+
+    >
       {/*	Body */}
       <input className={styles.todo__form__input} placeholder='Task Name' />
 
       {/*Form Footer */}
       <div className={styles.todo__form__footer}>
-        <p className={styles.todo__error}>Title is required</p>
+        {isError ? (<p className={styles.todo__error}>Title is required</p>) : null}
         <div className={styles.todo__form__buttons}>
-          <Button text='Cancel' active={false} />
-          <Button text={props.textSubmit} active={true} />
+          <Button
+            text='Cancel'
+            active={false}
+            type="button"
+            onClick={handleCancele} />
+          <Button
+            text={props.textSubmit}
+            active={true}
+            type='Submit' />
+          {/* <button type='button' onClick={handleCancel}> POC</button> */}
         </div>
       </div>
-    </form>
+    </form >
   );
 }
 
